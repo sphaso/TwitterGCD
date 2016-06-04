8b8d593faf7a96f06ad6a659e6e1c9392f79a5fb
Giovanni <sphaso@gmail.com>
2016-06-04 16:56:07 +0200

    loop - todo: turn into process

--- lib/twittergcd.ex
+++ lib/twittergcd.ex
@@ -2,8 +2,16 @@ defmodule Twittergcd do
 
   def start() do
     now = Calendar.DateTime.now_utc
-    TwitterWrap.get_mentions(now)
-      |> Enum.map(&((Messenger.handle_input &1) |> TwitterWrap.post_status))
+    loop(now)
+  end
+
+  defp loop(since) do
+    IO.puts("Loop")
+    TwitterWrap.get_mentions(since)
+              |> Enum.each(&((Messenger.handle_input &1) |> TwitterWrap.post_status))
+    now = Calendar.DateTime.now_utc
+    :timer.sleep(5 * 60000)
+    loop(now)
   end
 
 end
